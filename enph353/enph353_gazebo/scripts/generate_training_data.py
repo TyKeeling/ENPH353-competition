8f44c5b2a594e4d9efc30693b2b191735121675b
Tyler Keeling <tylerwkeeling@gmail.com>
2019-11-10 19:11:51 -0800

    Data generation env setup + documentation

---
+++ enph353/enph353_gazebo/scripts/generate_training_data.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# Generates training data for the ML plate reader
+
+# Inputs:
+# ./tranining_plates/ : big plate PNG images
+# ./backgrounds/ : screen captures from world with no other plates in it. 
+# command line: number of images to generate
+
+# Outputs: 
+# ./traning_images/XXXX_XX.png : images with big plate images transformed 
+# ./traning_images/XXXX_XX.xml : text document with the following information:
+
+# <image>
+#   <topleft>
+#     <exists>1</exists>
+#     <xcoord>14</xcoord>
+#     <ycoord>24</ycoord>
+#   </topleft>
+#   <topright>
+#     <exists>1</exists>
+#     <xcoord>14</xcoord>
+#     <ycoord>24</ycoord>
+#   </topright>
+#   <bottomleft>
+#     <exists>1</exists>
+#     <xcoord>14</xcoord>
+#     <ycoord>24</ycoord>
+#   </bottomleft>
+#   <bottomright>
+#     <exists>1</exists>
+#     <xcoord>14</xcoord>
+#     <ycoord>24</ycoord>
+#   </bottomright>
+# </image>
+# 
+# Where exists signifies the on page / off page constraint. 
+
+# On the matter of image generation:
+# Corners obviously must not be placed randomly. top left must be top right, bottom left must be bottom left
+# The deviance should be not too wonky either. We should generate images such that the corners
+# are only 30% error from the normal rectangle image. Top left will be randomly placed. 
+# Corners may also be placed off screen, which would result in a "0" for "exists" col.
+
+
+
+
+<note>
+<to>Tove</to>
+<from>Jani</from>
+<heading>Reminder</heading>
+<body>Don't forget me this weekend!</body>
+</note>
+
+
+
+
+import cv2
+import csv
+import numpy as np
+import os
+import pyqrcode
+import random
+import string
+
+from random import randint
+from PIL import Image, ImageFont, ImageDraw
