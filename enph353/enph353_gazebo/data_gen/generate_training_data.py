a7f44f0e88a9f9545b5a38aeca5f4c0e37a1eba7
Tyler Keeling <tylerwkeeling@gmail.com>
2019-11-13 17:34:21 -0800

    perspective transform works! Not sure how to find the corner points with the transform matrix

--- enph353/enph353_gazebo/data_gen/generate_training_data.py
+++ enph353/enph353_gazebo/data_gen/generate_training_data.py
@@ -50,6 +50,7 @@ import pyqrcode
 import random
 import string
 import glob
+import math
 
 from random import randint
 from PIL import Image, ImageFont, ImageDraw
@@ -59,94 +60,80 @@ IMAGE_WIDTH = 1280
 
 LOOP = 1
 
-# light purple color RGB: 100 100 202   HSV: 240 50.5 79.2
-# dark purple color RGB: 0 0 103        HSV: 240 100 40.4
+# Thank you to Matthew Earl https://github.com/matthewearl for the use of the below functions from 
+# https://github.com/matthewearl/deep-anpr/blob/master/gen.py , specifically the perspective transforms.
+# Copyright (c) 2016 Matthew Earl
 
+def euler_to_mat(yaw, pitch, roll):
+    # Rotate clockwise about the Y-axis
+    c, s = math.cos(yaw), math.sin(yaw)
+    M = np.matrix([[  c, 0.,  s],
+                      [ 0., 1., 0.],
+                      [ -s, 0.,  c]])
 
-# def find_coeffs(pa, pb): # old points, new points
-#     matrix = []
-#     for p1, p2 in zip(pa, pb):
-#         matrix.append([p1[0], p1[1], 1, 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1]])
-#         matrix.append([0, 0, 0, p1[0], p1[1], 1, -p2[1]*p1[0], -p2[1]*p1[1]])
+    # Rotate clockwise about the X-axis
+    c, s = math.cos(pitch), math.sin(pitch)
+    M = np.matrix([[ 1., 0., 0.],
+                      [ 0.,  c, -s],
+                      [ 0.,  s,  c]]) * M
 
-#     A = np.matrix(matrix, dtype=np.float)
-#     B = np.array(pb).reshape(8)
+    # Rotate clockwise about the Z-axis
+    c, s = math.cos(roll), math.sin(roll)
+    M = np.matrix([[  c, -s, 0.],
+                      [  s,  c, 0.],
+                      [ 0., 0., 1.]]) * M
 
-#     res = np.dot(np.linalg.inv(A.T * A) * A.T, B)
-#     return np.array(res).reshape(8)
+    return M
 
-# def shift(img):
-#     width, height = img.size
+def make_affine_transform(from_shape, to_shape, 
+                          min_scale, max_scale,
+                          scale_variation=1.0,
+                          rotation_variation=1.0,
+                          translation_variation=1.0):
+    out_of_bounds = False
 
-#     coeffs = find_coeffs(
-#         [(0, 0), (256, 0), (256, 256), (0, 256)],
+    from_size = np.array([[from_shape[1], from_shape[0]]]).T
+    to_size = np.array([[to_shape[1], to_shape[0]]]).T
 
-#         [(0, 150), (200, 0), (256, 256), (0, 256)])
+    scale = random.uniform((min_scale + max_scale) * 0.5 -
+                           (max_scale - min_scale) * 0.5 * scale_variation,
+                           (min_scale + max_scale) * 0.5 +
+                           (max_scale - min_scale) * 0.5 * scale_variation)
+    if scale > max_scale or scale < min_scale:
+        out_of_bounds = True
+    roll = random.uniform(-0.3, 0.3) * rotation_variation
+    pitch = random.uniform(-0.2, 0.2) * rotation_variation
+    yaw = random.uniform(-1.2, 1.2) * rotation_variation
 
-#     return img.transform((width, height), Image.PERSPECTIVE, coeffs,
-#         Image.BICUBIC)
+    # Compute a bounding box on the skewed input image (`from_shape`).
+    M = euler_to_mat(yaw, pitch, roll)[:2, :2]
+    h, w = from_shape
+    corners = np.matrix([[-w, +w, -w, +w],
+                            [-h, -h, +h, +h]]) * 0.5
+    skewed_size = np.array(np.max(M * corners, axis=1) -
+                              np.min(M * corners, axis=1))
 
-def order_points(pts):
-	# initialzie a list of coordinates that will be ordered
-	# such that the first entry in the list is the top-left,
-	# the second entry is the top-right, the third is the
-	# bottom-right, and the fourth is the bottom-left
-	rect = np.zeros((4, 2), dtype = "float32")
- 
-	# the top-left point will have the smallest sum, whereas
-	# the bottom-right point will have the largest sum
-	s = pts.sum(axis = 1)
-	rect[0] = pts[np.argmin(s)]
-	rect[2] = pts[np.argmax(s)]
- 
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
-	diff = np.diff(pts, axis = 1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[3] = pts[np.argmax(diff)]
- 
-	# return the ordered coordinates
-	return rect
+    # Set the scale as large as possible such that the skewed and scaled shape
+    # is less than or equal to the desired ratio in either dimension.
+    scale *= np.min(to_size / skewed_size)
 
-def four_point_transform(image, pts):
-    # SOURCE: Adrian Rosebrock @ https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
-	# obtain a consistent order of the points and unpack them
-	# individually
-	rect = order_points(pts)
-	(tl, tr, br, bl) = rect
- 
-	# compute the width of the new image, which will be the
-	# maximum distance between bottom-right and bottom-left
-	# x-coordiates or the top-right and top-left x-coordinates
-	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-	maxWidth = max(int(widthA), int(widthB))
- 
-	# compute the height of the new image, which will be the
-	# maximum distance between the top-right and bottom-right
-	# y-coordinates or the top-left and bottom-left y-coordinates
-	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-	maxHeight = max(int(heightA), int(heightB))
- 
-	# now that we have the dimensions of the new image, construct
-	# the set of destination points to obtain a "birds eye view",
-	# (i.e. top-down view) of the image, again specifying points
-	# in the top-left, top-right, bottom-right, and bottom-left
-	# order
-	dst = np.array([
-		[0, 0],
-		[maxWidth - 1, 0],
-		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
- 
-	# compute the perspective transform matrix and then apply it
-	M = cv2.getPerspectiveTransform(rect, dst)
-	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
- 
-	# return the warped image
-	return warped
+    # Set the translation such that the skewed and scaled image falls within
+    # the output shape's bounds.
+    trans = (np.random.random((2,1)) - 0.5) * translation_variation
+    trans = ((2.0 * trans) ** 5.0) / 2.0
+    if np.any(trans < -0.5) or np.any(trans > 0.5):
+        out_of_bounds = True
+    trans = (to_size - skewed_size * scale) * trans
+
+    center_to = to_size / 2.
+    center_from = from_size / 2.
+
+    M = euler_to_mat(yaw, pitch, roll)[:2, :2]
+    M *= scale
+    M = np.hstack([M, trans + center_to - M * center_from])
+
+    return M, out_of_bounds, trans
+
 
 def main():
     # import backgrounds as a list, increment starting at 0 
@@ -157,20 +144,38 @@ def main():
     plates = [cv2.imread(file) for file in glob.glob("./training_plates/*.png")]
     platelabel = [string.split("/")[2] for string in plate_location]
 
-    pts = [0, 10, 20, 30]
-    trans = four_point_transform(backgrounds[0], pts)
-
-    cv2.imshow("Image window",  )
-    cv2.waitKey(0)
-
 
     for i in range(0, LOOP):
-         for plate in plates:
+         for plate in plates[0:10]:
 
+			bg = backgrounds[iback]
 
-             
+			plate = cv2.resize(plate, (plate.shape[0]/2, plate.shape[1])) # scaling down by 2
 
-             iback = (iback + 1) % len(backgrounds)
+			M, out_of_bounds, trans = make_affine_transform(
+                    from_shape=(plate.shape[0], plate.shape[1]),
+                    to_shape=(bg.shape[0], bg.shape[1]),
+					min_scale=0.6,
+					max_scale=0.875,
+					rotation_variation=1.0,
+					scale_variation=1.5,
+					translation_variation=1.2)
+
+			center = (trans[0]+bg.shape[1]/2, trans[1]+bg.shape[0]/2)
+			print center
+
+			plate = cv2.warpAffine(plate, M, (bg.shape[1], bg.shape[0]))
+			plate = cv2.circle(plate, center, 1, (0,0,255), 20)          # this is the center, how to get corners?
+
+			plate_bw = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+			val, plate_mask = cv2.threshold(plate_bw,240,255,cv2.THRESH_BINARY)
+	
+
+    		out = plate * plate_mask + bg * (1.0 - plate_mask)
+			cv2.imshow("Image window", plate)
+			cv2.waitKey(0)
+
+			iback = (iback + 1) % len(backgrounds)
 
 if __name__ == '__main__':
     main()
