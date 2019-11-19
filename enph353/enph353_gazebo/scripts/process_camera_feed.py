87310d30a7d5ee2a05da5fda27489acd650226fc
Tyler Keeling <tylerwkeeling@gmail.com>
2019-11-19 14:42:13 -0800

    Rect Box on most plates Some being ommitted. Good progress

--- enph353/enph353_gazebo/scripts/process_camera_feed.py
+++ enph353/enph353_gazebo/scripts/process_camera_feed.py
@@ -35,43 +35,51 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        output = purplemask(cv_image, stripes=True)
-        image_message = self.bridge.cv2_to_imgmsg(output, encoding="8UC1") #bgr8 or 8UC1
+        output = colormask_contour(cv_image)
+        image_message = self.bridge.cv2_to_imgmsg(output, encoding="bgr8") #bgr8 or 8UC1
         self.image_out.publish( image_message )
 
+def notEdges(x, y, w, h, im_w=1280, im_h=720):
+    if x <= 0:
+        return False
+    if y <= 0:
+        return False
+    if (x + w) >= im_w:
+        return False
+    if (y + h) >= im_h:
+        return False
+    return True
 
 def colormask_contour(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    # output = np.zeros(img.shape)
-    
-                        # Hue Saturation Value(Brightness)
+
+    # HSV MASKS
     lower_red = np.array([0,0,90])
     upper_red = np.array([255,50,200])
+
+    lower_black = np.array([0,0,0])
+    upper_black = np.array([255,10,20])
     
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    colormask = cv2.inRange(hsv, lower_red, upper_red) # actually white lol
+    blackmask = cv2.inRange(hsv,lower_black,upper_black)
+    bwmask = cv2.bitwise_or(colormask, blackmask)
+
+    boxmask = purplemask(img, stripes=True)
+    mask = cv2.bitwise_and(bwmask, boxmask)
 
     kernel = np.ones((2,2),np.uint8)
-    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) # denoises the mask
 
     _, contours, h = cv2.findContours(opening, 1, 2)
 
     for cnt in contours:
-        cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+        #cv2.drawContours(img,[cnt],0,(0,0,255),-1)
         approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
-        #print len(approx)
-        # if len(approx)==5:
-        #     print "pentagon"
-        #     cv2.drawContours(img,[cnt],0,255,-1)
-        # elif len(approx)==3:
-        #     print "triangle"
-        #     cv2.drawContours(img,[cnt],0,(0,255,0),-1)
-
         if len(approx)==4:
             x,y,w,h = cv2.boundingRect(cnt)
-            if w > 50 and h > 50 and cv2.contourArea(cnt) > 500:
+            h = int(h*1.37)
+            if w > 80 and h > 100 and notEdges(x,y,w,h):
                 cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-                #print cv2.moments(cnt)
-                cv2.drawContours(img,[cnt],0,(0,0,255),-1)
 
     return img
 
@@ -87,16 +95,21 @@ def purplemask(img, stripes=False):
     #Generate the janky mask around the purple values s.t. we can only find plates. 
     if stripes:
         overmask = np.zeros(purplemask.shape, np.dtype('uint8'))
+        _, contours, h = cv2.findContours(opening, 1, 2)
 
-        for i in range(overmask.shape[0]): #row by row
-            for pixel in np.nditer(purplemask[i,1]):
-                if pixel != 0:
-                    overmask[i] = 255 #make this range white
-                    break
+        stretchbox = 100
+        cutpix = 40 # hopefully to get rid of the bottom part of the thing
+
+        for cnt in contours:
+            #cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+            x,y,w,h = cv2.boundingRect(cnt)
+            x -= stretchbox
+            w += 2*stretchbox
+            h -= cutpix
+            cv2.rectangle(overmask,(x,y),(x+w,y+h),(255), cv2.FILLED)
 
         return overmask
     return opening
-
 
 # Decent idea but this is insanely slow :(
 def cluster(img): #https://towardsdatascience.com/introduction-to-image-segmentation-with-k-means-clustering-83fd0a9e2fc3
