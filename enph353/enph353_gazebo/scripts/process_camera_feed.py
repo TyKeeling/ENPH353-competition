ce0e47148ca21ac3e4ead962613b4406c08799e8
Tyler Keeling <tylerwkeeling@gmail.com>
2019-11-19 13:33:04 -0800

    Looking at purplemask for better plate filtering

--- enph353/enph353_gazebo/scripts/process_camera_feed.py
+++ enph353/enph353_gazebo/scripts/process_camera_feed.py
@@ -35,29 +35,29 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        output = process_image(cv_image)
-        image_message = self.bridge.cv2_to_imgmsg(output, encoding="bgr8")
+        output = purplemask(cv_image, stripes=True)
+        image_message = self.bridge.cv2_to_imgmsg(output, encoding="8UC1") #bgr8 or 8UC1
         self.image_out.publish( image_message )
 
 
-def process_image(img):
-
+def colormask_contour(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # output = np.zeros(img.shape)
     
                         # Hue Saturation Value(Brightness)
     lower_red = np.array([0,0,90])
-    upper_red = np.array([255,10,200])
+    upper_red = np.array([255,50,200])
     
     mask = cv2.inRange(hsv, lower_red, upper_red)
 
     kernel = np.ones((2,2),np.uint8)
     opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-
-    image, contours, h = cv2.findContours(opening,1,2)
+    _, contours, h = cv2.findContours(opening, 1, 2)
 
     for cnt in contours:
-        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+        cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+        approx = cv2.approxPolyDP(cnt,0.1*cv2.arcLength(cnt,True),True)
         #print len(approx)
         # if len(approx)==5:
         #     print "pentagon"
@@ -73,17 +73,44 @@ def process_image(img):
                 #print cv2.moments(cnt)
                 cv2.drawContours(img,[cnt],0,(0,0,255),-1)
 
-    # image, contours, hierarchy = cv2.findContours(opening,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-    # output = cv2.drawContours(image, contours, -1, (0,255,0), 3)
-    
-    #res = cv2.bitwise_and(img, img, mask= opening)
-
-    #cv2.imshow("Image window", edged)
-    #cv2.waitKey(0)
-
     return img
 
+def purplemask(img, stripes=False):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_purple = np.array([120,30,30])
+    upper_purple = np.array([130,255,255])
+    purplemask = cv2.inRange(hsv, lower_purple, upper_purple)
+
+    kernel = np.ones((2,2),np.uint8)
+    opening = cv2.morphologyEx(purplemask, cv2.MORPH_OPEN, kernel)
+
+    #Generate the janky mask around the purple values s.t. we can only find plates. 
+    if stripes:
+        overmask = np.zeros(purplemask.shape, np.dtype('uint8'))
+
+        for i in range(overmask.shape[0]): #row by row
+            for pixel in np.nditer(purplemask[i,1]):
+                if pixel != 0:
+                    overmask[i] = 255 #make this range white
+                    break
+
+        return overmask
+    return opening
+
+
+# Decent idea but this is insanely slow :(
+def cluster(img): #https://towardsdatascience.com/introduction-to-image-segmentation-with-k-means-clustering-83fd0a9e2fc3
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    vectorized = hsv.reshape((-1,3))
+    vectorized = np.float32(vectorized)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 3
+    attempts=10
+    ret,label,center=cv2.kmeans(vectorized,K,None,criteria,attempts,cv2.KMEANS_PP_CENTERS)
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    result_image = res.reshape((img.shape))
+    return result_image
 
 def main(args):
     rospy.init_node('image_converter', anonymous=True)
@@ -94,7 +121,6 @@ def main(args):
     except KeyboardInterrupt:
         print("Shutting down")
     cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     main(sys.argv)
